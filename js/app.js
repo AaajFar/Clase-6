5b5e8414b84df23ecd60f91daa02e3c49ff47588
Ariel Farbiarz <arielfarbiarz@gmail.com>
2022-03-21 20:07:00 -0300

    commit sound

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("started...");
+import Button from "./button.js";
+import Imagen from "./imagen.js";
+import Sound from "./sound.js";
+
+let myDiv = document.querySelector("#myApp");
+
+let myButton = new Button("Click me!");
+
+myDiv.innerHTML = myButton.render();
+
+
+let myImagen = new Imagen("./data/logo_OK.png");
+
+myDiv.innerHTML += myImagen.render();
+
+let mySound = new Sound("https://www.w3schools.com/tags/horse.ogg");
+
+myDiv.innerHTML += mySound.render();
